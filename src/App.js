a4e7710ec2ed86c7fb613e6fa7fe2889fe7f0112
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-30 14:56:47 -0600

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+import {Routes, Route} from "react-router-dom";
 import './App.css';
+import HomePageComponent from "./components/Home/HomePage";
+
+
 
 function App() {
+ 
   return (
     <div className="App">
+      <Routes>
+        <Route path='*' element={<HomePageComponent />}></Route>
+      </Routes>
       
     </div>
   );
+
+ 
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/css/index.css';
 import App from './App';
-
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
