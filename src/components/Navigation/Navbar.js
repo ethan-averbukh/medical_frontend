4b2eaa26d1e7e708e934f77496b8076273af635c
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-31 23:19:07 -0600

    Added bootstrap classes

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,18 +1,36 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBarComponent = () => {
-    return (
-        <nav className="nav">
-            <div className="nav-brand">
-                <Link to='/' className="nav-link-0">Home</Link>
-            </div>
-            <div className="nav-links">
-                <Link to='/Notes' className="nav-link-1">Notes</Link>
-                <Link to='/Journal' className="nav-link-2">Journal</Link>
-            </div>
-        </nav>
-    )
-}
-
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className=" navbar-brand">
+          Home
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link to="/Notes" className="nav-link">
+            Notes
+          </Link>
+          <Link to="/Journal" className="nav-link">
+            Journal
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBarComponent;
