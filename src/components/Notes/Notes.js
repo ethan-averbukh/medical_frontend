221e3a8bd4d5c234dc3578a208a0691e1d258391
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-31 23:19:25 -0600

    Added symptom form submission.

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -2,29 +2,50 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 
 
+
 const NotesComponent = ({handleNotesSubmission}) => {
-    const [newNote, setNewNote] = useState({})
+    const [value, setValue] = useState(0);
+    const [newNote, setNewNote] = useState({
+        'Date': new Date().toLocaleString(),
+        'symptom': '',
+        'severity': 0,
+        'medications': '',
+        'activities': '',
+        'foods': ''
+    })
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        handleNotesSubmission(newNote);
         navigate('/')
     }
     const handleChange = (event) => {
-        console.log(event.target.value)
+        if(event.target.name === 'severity'){
+            setValue(event.target.value);
+        }
+        setNewNote((prevState) => {
+            const newEntry = {
+                ...prevState,
+                [event.target.name]: event.target.value
+            };
+            return newEntry
+        })
+        
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div className="notes">
+            <form className="notes-form" onSubmit={handleSubmit}>
                 <label htmlFor='symptom'>Symptom</label>
                 <input type="text" id='symptom' name='symptom' onChange={handleChange}/>
                 <label htmlFor="severity">Severity</label>
-                <input type="range" min='1' max='10' defaultValue='5' name='severity' id='Severity' onChange={handleChange}/>
+                <input type="range" min='1' max='10' defaultValue='1' name='severity' id='Severity' onChange={handleChange}/>
+                <label htmlFor="severity">Value:{value}</label>
                 <label htmlFor="activities">Daily Activities</label>
-                <textarea name="activities" id="activities" cols="30" rows="10" onChange={handleChange}></textarea>
-                <label htmlFor="foods">Consumables</label>
-                <textarea name="foods" id="foods" cols="30" rows="10" defaultValue={"What have you eaten and/or drank today?"} onChange={handleChange}/>
-                <input type="submit" value="Submit" />
+                <textarea name="activities" id="activities" cols={30} rows={10} onChange={handleChange}></textarea>
+                <label htmlFor="foods">Daily Food Tracker</label>
+                <textarea name="foods" id="foods" cols={30} rows={10} defaultValue={""} onChange={handleChange}/>
+                <input type="submit" value="New Entry" />
             </form>
         </div>
     )
