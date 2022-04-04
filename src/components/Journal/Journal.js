355ccd5f340f831d8ab3d597a8f3730a55c33f6c
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-03 21:49:21 -0600

    Updated to be an accordion.

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,35 +1,56 @@
-import {useState, useEffect} from 'react';
-import { useNavigate } from 'react-router';
+import { useState, useEffect } from "react";
+import { convertDate } from "../../TimeConverter";
+import { getJournalAPICall } from "../../apis/journalsAPI";
+import NotesList from "../Notes/createNotes";
+const JournalComponent = () => {
+  const [manyJournalEntries, setManyJournalEntries] = useState([]);
+  const [lengthOfJournalEntries, setLengthOfJournalEntries] = useState(0);
+  useEffect(async () => {
+    //Retrieve all of the journal entries and set them to the state
+    const response = getJournalAPICall();
 
-const JournalComponent = ({journalEntries}) => {
-    const navigate = useNavigate();
-    let listofEntries;
-    const [newEntries, setNewEntries] = useState([]);
-    const [newEntryCount, setNewEntryCount] = useState(0);
-
-    useEffect(()=>{
-        if(journalEntries){
-            setNewEntries(journalEntries);
-        }
-        console.log(newEntries)
-      
-    }, [newEntryCount])
-    const handleClick = () => {
-        navigate('/')
+    {
+      response && setManyJournalEntries(response.data);
     }
-    listofEntries = newEntries.map((item,index)=>{
-        return (
-            <div key={index}>
-                {item.Symptom}
+  }, [lengthOfJournalEntries]);
+
+  const checkJournalEntriesLength = () => {};
+  return (
+    <div className="accordion" id="Accordion">
+      {manyJournalEntries &&
+        manyJournalEntries.map((item, index) => {
+
+          return (
+            <div className="accordion-item" key={item._id}>
+              <h2 className="accordion-header" id={`Journal${index}`}>
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${index}`}
+                  aria-expanded="true"
+                  aria-controls={`collapse${index}`}
+                >
+                  {item.title}
+                </button>
+              </h2>
+              <div
+                id={`collapse${index}`}
+                className="accordion-collapse"
+                aria-labelledby={`Journal${index}`}
+                data-bs-parent="#Accordion"
+              >
+                <div className="accordion-body">
+                    <NotesList notes={item.notes}/>
+                </div>
+              </div>
+              <p>{convertDate(item.Date).year}</p>
+              <p>{convertDate(item.Date).time}</p>
             </div>
-        )
-    })
-    return (
-        <div>
-            {listofEntries}
-            <input type="submit" onClick={handleClick}/>
-        </div>
-    )
-}
+          );
+        })}
+    </div>
+  );
+};
 
 export default JournalComponent;
