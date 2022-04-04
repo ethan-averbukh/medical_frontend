3fbc671cf6087912e368d16a92032e58d694f21d
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 15:38:01 -0600

    Removed routes from App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import {Routes, Route} from 'react-router-dom';
 import "./App.css";
 import HomePageComponent from "./components/Home/HomePage";
+import IntroComponent from './components/Home/IntroductionPage';
 
 
 function App() {
 
   return (
     <div className="App">
-      <HomePageComponent />
+    
     </div>
   );
 }
