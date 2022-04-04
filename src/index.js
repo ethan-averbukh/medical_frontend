3dc4450b75f7f9fd71296e485df3ca5a4c1eb202
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-03 21:41:51 -0600

    Removed AuthProvider for now. and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/css/index.css';
-import App from './App';
-import { HashRouter as Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/css/index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
-  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
-  document.getElementById('root')
+  ,
+  document.getElementById("root")
 );
-
