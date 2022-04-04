833712b800dd2618688d9b0d3d1775ac5ecfc09a
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 00:13:01 -0600

    Removed unnecessary code.

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,27 +1,13 @@
-import { Routes, Route } from "react-router-dom";
 import NavBarComponent from "../Navigation/Navbar";
 import JournalComponent from "../Journal/Journal";
-import NotesComponent from "../Notes/Notes";
-import { useEffect, useState } from "react";
-import { getNotesAPICall } from "../../apis/notesAPI";
+
 
 const HomePageComponent = () => {
-  const [journalEntry, setJournalEntry] = useState({});
-  const [notesEntries, setNotesEntries] = useState([]);
 
-  // useEffect(async () => {
-  //   const notesResponse = getNotesAPICall();
-  //   setNotesEntries(notesResponse.data);
-  // }, [journalEntry]);
 
-  // const sumbitANote = (noteSubmission) => {
-  //   setJournalEntry(noteSubmission);
-  //   console.log(noteSubmission);
-  // };
   return (
     <div>
       <NavBarComponent />
-      {/* <NotesComponent handleNotesSubmission={sumbitANote} /> */}
       <JournalComponent />
     </div>
   );
