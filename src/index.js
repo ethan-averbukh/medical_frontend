c19d6200e84254397ed35e7862e2625d715609f2
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 15:44:39 -0600

    Added routes.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Routes, Route } from "react-router-dom";
 import "./styles/css/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import IntroComponent from "./components/Home/IntroductionPage";
+import HomePageComponent from "./components/Home/HomePage";
 
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
+  <Router>
     <React.StrictMode>
       <App />
+      <Routes>
+        <Route path="/" element={<IntroComponent />}/>
+        <Route path="/intro" element={<IntroComponent />} />
+        <Route path="/home" element={<HomePageComponent />} />
+      </Routes>
     </React.StrictMode>
-  ,
+  </Router>,
   document.getElementById("root")
 );
