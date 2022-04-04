c53c6830f4e2b72c1077cd3d998a838c17bd7351
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 10:11:28 -0600

    Updated accordion to toggle individual items.

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,22 +1,55 @@
 import { useState, useEffect } from "react";
 import { convertDate } from "../../TimeConverter";
 import { getJournalAPICall } from "../../apis/journalsAPI";
+
 import NotesList from "../Notes/createNotes";
-const JournalComponent = () => {
+const JournalComponent = ({ journalEntry }) => {
   const [manyJournalEntries, setManyJournalEntries] = useState([]);
   const [lengthOfJournalEntries, setLengthOfJournalEntries] = useState(0);
   useEffect(async () => {
     //Retrieve all of the journal entries and set them to the state
     // const response = getJournalAPICall();
-
     // {
     //   response && setManyJournalEntries(response.data);
     // }
   }, [lengthOfJournalEntries]);
-
   return (
     <div className="accordion" id="Accordion">
-      {manyJournalEntries &&
+      {journalEntry &&
+        journalEntry.map((item, index) => {
+          console.log(item);
+          return (
+            <div className="accordion-item " key={item._id}>
+              <h2 className="accordion-header" id={`Journal${index}`}>
+                <button
+                  className="accordion-button "
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapse${index}`}
+                  aria-expanded="false"
+                  aria-controls={`collapse${index}`}
+                >
+                  {item.title}
+                </button>
+              </h2>
+              <div
+                id={`collapse${index}`}
+                className="accordion-collapse collapse"
+                aria-labelledby={`Journal${index}`}
+                data-bs-parent="#Accordion"
+              >
+                <div className="accordion-body">
+                  {item.notes ? (
+                    <NotesList notes={item.notes} />
+                  ) : (
+                    <NotesList notes={undefined} />
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      {/* {manyJournalEntries &&
         manyJournalEntries.map((item, index) => {
 
           return (
@@ -47,7 +80,7 @@ const JournalComponent = () => {
               <p>{convertDate(item.Date).time}</p>
             </div>
           );
-        })}
+        })} */}
     </div>
   );
 };
