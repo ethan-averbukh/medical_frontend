45140ddf7b86f2a97a9da65dd5780a23d5ca17c2
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 15:44:27 -0600

    removed object for return.

--- src/components/Notes/createNotes.js
+++ src/components/Notes/createNotes.js
@@ -6,14 +6,15 @@ export const NotesList = (notes) => {
   if (notes != undefined) {
     notesList = notes.notes.map((item, index) => {
       return (
-        <div>
+        <div className='d-grid gap-2'>
           <button
             key={index}
+            className='btn btn-outline-secondary'
             data-bs-toggle="modal"
             data-bs-target="#noteModal"
           >
-            <h4>{item.Symptom}</h4>
-            <h5>{item.Date}</h5>
+            <h4>{item.symptom}</h4>
+            <h5>{item.date}</h5>
           </button>
           <NotesComponent noteValues={item} />
         </div>
