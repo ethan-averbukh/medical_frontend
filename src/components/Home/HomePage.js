5a0969dc7a8db52e163eadb495cbd5662415df22
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 15:41:44 -0600

    updated classname and journal data.

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,13 +1,14 @@
-import NavBarComponent from "../Navigation/Navbar";
-import JournalComponent from "../Journal/Journal";
+import NavBarComponent from "./../Navigation/Navbar";
+import JournalComponent from "./../Journal/Journal";
 import data from './../../journalData.json';
-
+import strings from './../../strings.json';
 const HomePageComponent = () => {
 
 
   return (
     <div>
       <NavBarComponent />
+      <h3 className="home-journal-header">{strings.journalsHeader}</h3>
       <JournalComponent journalEntry={data.journal}/>
     </div>
   );
