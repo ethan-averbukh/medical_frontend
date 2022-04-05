1d8dcc6e62578ae766d43607c7480a927245a479
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-05 08:34:42 -0600

    Adjusted put api.

--- src/apis/journalsAPI.js
+++ src/apis/journalsAPI.js
@@ -3,24 +3,44 @@ import axios from 'axios';
 
 //Journal API Calls
 const getJournalAPICall = async () => {
-    const journals = await axios.get("https://protected-brook-20008.herokuapp.com/Journals");
+    let journals;
+    try {
+        journals = await axios.get("http://localhost:3000/Journals");
+    } catch (error) {
+        console.log(error)
+    }
+   
     return journals;
 }
 
+const deleteJournalAPICall = async (journalID) => {
+    try {
+        await axios.delete(`http://localhost:3000/Journals/${journalID}`)
+    } catch (error) {
+        
+    }
+}
 const postJournalAPICall = async (journal) => {
-    const journalPost = await axios.post("https://protected-brook-20008.herokuapp.com/Journals", {
+    const journalPost = await axios.post("http://localhost:3000/Journals", {
         "title": journal.title,
         "Date": journal.Date,
         "Notes": journal.Notes
     })
 }
 
-const putJournalAPICall = async (journal) => {
-    const journalPost = await axios.put(`https://protected-brook-20008.herokuapp.com/Journals/${journal._id}`, journal)
+const putJournalAPICall = async (journal, note) => {
+    const journalPost = await axios.put(`http://localhost:3000/Journals/${journal._id}`,
+    {
+        title: journal.title,
+        date: journal.date,
+        notes: note._id
+    }
+    )
 }
 
 
 export {
+    deleteJournalAPICall,
     getJournalAPICall,
     postJournalAPICall,
     putJournalAPICall
