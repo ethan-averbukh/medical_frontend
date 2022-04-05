1a01d2594099ef6ad2979f302464da7ac6c2fba1
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 22:03:24 -0600

    Switched back to browserrouter.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/css/index.css";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
