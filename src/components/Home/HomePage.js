707d72945d26347e69efad9f9be7127e6d2e525c
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-05 08:35:58 -0600

    Added prop journals and totalJournals.

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,15 +1,15 @@
+import {useState,useEffect} from 'react';
 import NavBarComponent from "./../Navigation/Navbar";
 import JournalComponent from "./../Journal/Journal";
-import data from './../../journalData.json';
-import strings from './../../strings.json';
-const HomePageComponent = () => {
 
+import strings from './../../strings.json';
+const HomePageComponent = ({journals}) => {
 
   return (
     <div>
       <NavBarComponent />
       <h3 className="home-journal-header">{strings.journalsHeader}</h3>
-      <JournalComponent journalEntry={data.journal}/>
+      <JournalComponent totalJournals={journals}/>
     </div>
   );
 };
