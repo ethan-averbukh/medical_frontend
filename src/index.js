5b0dcf26cf6fa5b07a6654c67abbee49321f91df
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-04 18:20:59 -0600

    changed browserrouter to hashrouter.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Routes, Route } from "react-router-dom";
 import "./styles/css/index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import IntroComponent from "./components/Home/IntroductionPage";
-import HomePageComponent from "./components/Home/HomePage";
+import { HashRouter as Router } from "react-router-dom";
+
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
@@ -14,7 +12,6 @@ ReactDOM.render(
   <Router>
     <React.StrictMode>
       <App />
-    
     </React.StrictMode>
   </Router>,
   document.getElementById("root")
