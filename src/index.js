61b3a2311f6617566b68103a821e2d483b847fe4
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-05 17:10:18 -0600

    Added then removed Auth0.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./styles/css/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    
   </Router>,
   document.getElementById("root")
 );
