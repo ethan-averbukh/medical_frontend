6fb1e9f6f9673a0bf607e24d3cda739e98938225
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-05 08:37:03 -0600

    Changed journal prop to Notes to be full object.

--- src/components/Journal/Journal.js
+++ src/components/Journal/Journal.js
@@ -1,20 +1,27 @@
 import { useState, useEffect } from "react";
-
+import {
+  deleteJournalAPICall,
+  getJournalAPICall,
+} from "./../../apis/journalsAPI";
 import NotesList from "../Notes/createNotes";
-const JournalComponent = ({ journalEntry }) => {
+const JournalComponent = ({ totalJournals }) => {
   const [manyJournalEntries, setManyJournalEntries] = useState([]);
-  const [lengthOfJournalEntries, setLengthOfJournalEntries] = useState(0);
+  const [deletedItems, setDeletedItems] = useState(0);
+
   useEffect(async () => {
-    //Retrieve all of the journal entries and set them to the state
-    // const response = getJournalAPICall();
-    // {
-    //   response && setManyJournalEntries(response.data);
-    // }
-  }, [lengthOfJournalEntries]);
+    // Retrieve all of the journal entries and set them to the state
+    const response = await getJournalAPICall();
+    setManyJournalEntries(response.data);
+  }, [deletedItems, totalJournals]);
+
+  const handleDelete = (itemID) => {
+    deleteJournalAPICall(itemID);
+  };
   return (
     <div className="accordion" id="Accordion">
-      {journalEntry &&
-        journalEntry.map((item, index) => {
+      {manyJournalEntries.data &&
+        manyJournalEntries.data.map((item, index) => {
+          console.log(item._id);
           return (
             <div className="accordion-item " key={item._id}>
               <h2 className="accordion-header" id={`Journal${index}`}>
@@ -27,6 +34,16 @@ const JournalComponent = ({ journalEntry }) => {
                   aria-controls={`collapse${index}`}
                 >
                   {item.title}
+                  <div
+                    id="Delete"
+                    className="btn btn-danger "
+                    onClick={() => {
+                      handleDelete(item._id);
+                      setDeletedItems(deletedItems + 1);
+                    }}
+                  >
+                    Delete
+                  </div>
                 </button>
               </h2>
               <div
@@ -36,48 +53,16 @@ const JournalComponent = ({ journalEntry }) => {
                 data-bs-parent="#Accordion"
               >
                 <div className="accordion-body">
-                  {item.notes ? (
-                    <NotesList notes={item.notes} />
+                  {item.notes.length ? (
+                    <NotesList notes={item.notes} journal={item}/>
                   ) : (
-                    <NotesList notes={undefined} />
+                    <NotesList notes={[]} journal={item}/>
                   )}
                 </div>
               </div>
             </div>
           );
         })}
-      {/* {manyJournalEntries &&
-        manyJournalEntries.map((item, index) => {
-
-          return (
-            <div className="accordion-item" key={item._id}>
-              <h2 className="accordion-header" id={`Journal${index}`}>
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target={`#collapse${index}`}
-                  aria-expanded="true"
-                  aria-controls={`collapse${index}`}
-                >
-                  {item.title}
-                </button>
-              </h2>
-              <div
-                id={`collapse${index}`}
-                className="accordion-collapse"
-                aria-labelledby={`Journal${index}`}
-                data-bs-parent="#Accordion"
-              >
-                <div className="accordion-body">
-                   { item.notes ? <NotesList notes={item.notes} /> : <NotesList notes={undefined}/>}
-                </div>
-              </div>
-              <p>{convertDate(item.Date).year}</p>
-              <p>{convertDate(item.Date).time}</p>
-            </div>
-          );
-        })} */}
     </div>
   );
 };
