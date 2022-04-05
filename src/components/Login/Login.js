bc500f12f3043f885a1a1878c2f6e3ad79128047
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-04-05 17:08:30 -0600

    Refactored to include Login button.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,11 @@
+import LoginButton from "./LoginButton"
 
 const Login = () => {
     return(
-        <div className="login-wrapper">
-            <h1>Please Log In</h1>
-        <form>
-            <label htmlFor="username">
-                <p>UserName</p>
-                <input type="text" />
-            </label>
-            <label htmlFor="password">
-                <p>Passowrd</p>
-                <input type="password"/>
-            </label>
-            <div>
-                <button type="submit">Login</button>
-            </div>
-        </form>
+        <div className="login">
+            <LoginButton />
         </div>
     )
 }
+
+export default Login;
---
+++ src/components/Login/Logout.js
@@ -0,0 +1,11 @@
+
+
+const LogOutPage = () => {
+    return (
+        <div className="logout">
+            <p>You have been successfully logged out! Come back Soon!</p>
+        </div>
+    )
+}
+
+export default LogOutPage;
